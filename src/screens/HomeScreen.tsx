e9c7fa372afd02df42345f062578aa7b605b0524
Esmerlin Elivo <eselivo@contractor.appsbees.com>
2021-12-01 12:08:47 -0400

    first commit

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { StackScreenProps } from '@react-navigation/stack'
+import { Image, Text, View } from 'react-native'
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+import { styles } from '../theme/appTheme';
+
+interface Props extends StackScreenProps<any, any> {}
+
+const HomeScreen = ({ navigation }: Props) => {
+
+    const { top } = useSafeAreaInsets();
+
+    return (
+        <View>
+
+            <Image 
+                source={ require('../assets/pokebola.png')}
+                style={ styles.pokeBallBG }
+            />
+            <Text style={{
+                ...styles.title,
+                ...styles.globalMargin,
+                top: top + 20,
+            }}>
+                Pokedex
+            </Text>
+            
+        </View>
+    )
+}
+
+export default HomeScreen
