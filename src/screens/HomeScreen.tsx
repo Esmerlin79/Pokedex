d26577244a2d16f690da9bf986d723264bfcb7f8
Esmerlin Elivo <eselivo@contractor.appsbees.com>
2021-12-01 14:11:31 -0400

    add custom hook usePokemonPaginated

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -5,12 +5,14 @@ import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import Icon from 'react-native-vector-icons/Ionicons';
 
 import { styles } from '../theme/appTheme';
+import usePokemonPaginated from '../hooks/usePokemonPaginated';
 
 interface Props extends StackScreenProps<any, any> {}
 
 const HomeScreen = ({ navigation }: Props) => {
 
     const { top } = useSafeAreaInsets();
+    const { isLoading, simplePokemonList } = usePokemonPaginated();
 
     return (
         <View>
